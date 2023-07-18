ff650fcdb5aa35e57676237b6426ea06e55f3b4f
Alfie Collins <alfiecollins44@gmail.com>
2023-07-18 23:38:55 +0100

    DOM manip, classes, public, private, readonly

--- src/app.ts
+++ src/app.ts
@@ -8,20 +8,6 @@
 
 //const form = document.querySelector('form')!;
 
-//***CLASSES ***/
-
-class Invoice {
-    constructor(
-        readonly client: string,
-        private details: string,
-        public amount: number,
-    ){}
-
-    format(){
-        return `${this.client} owes £${this.amount} for ${this.details}`
-    }
-}
-
 const invOne = new Invoice('mario','work on mario website',250);
 const invTwo = new Invoice('luigi','work on luigi website',300);
 
@@ -33,7 +19,6 @@ invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format());
 })
 
-invOne.client = 'yoshi';
 invTwo.amount = 400;
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
